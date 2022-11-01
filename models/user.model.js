47f7cc1d505cf4749fd48db723af031534a1de59
sjjtu <johntu@live.de>
2022-11-01 13:43:14 +0100

    add user to board

--- models/user.model.js
+++ models/user.model.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  surname: { type: String, required: true },
-  lastname: { type: String, required: true},
-  email: { type: String, required: true}, // TODO: validate format
+  name: { type: String, required: true },
   boards: { type: [Schema.Types.ObjectId] },
 }, {
   timestamps: true,
